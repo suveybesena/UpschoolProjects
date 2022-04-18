34b5a6eaa221d52998f2d16db4b07c7facc19171
Suveybe Sena Kucuk <suveybesena@gmail.com>
2022-04-19 00:51:45 +0300

    add animation

--- app/src/main/java/com/suveybesena/upschoolprojects/MainActivity.kt
+++ app/src/main/java/com/suveybesena/upschoolprojects/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.suveybesena.upschoolprojects
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AppCompatActivity
+import com.suveybesena.upschoolprojects.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.materialButton.setOnClickListener {
+            val customAnimation: Animation = AnimationUtils.loadAnimation(this, R.anim.custom_anim)
+            binding.bvAction.startAnimation(customAnimation)
+        }
     }
 }
